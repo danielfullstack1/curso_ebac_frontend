43d6d73a2c5c8bfcce20ea797f32ad928ebe77b4
danielfullstack1 <emailmktmorais@gmail.com>
2024-12-28 15:36:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('numberForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio automático do formulário
+  
+    const campoA = parseFloat(document.getElementById('campoA').value);
+    const campoB = parseFloat(document.getElementById('campoB').value);
+    const mensagem = document.getElementById('mensagem');
+  
+    if (campoB > campoA) {
+      mensagem.textContent = 'Formulário válido: Campo B é maior que Campo A.';
+      mensagem.style.color = 'green';
+    } else {
+      mensagem.textContent = 'Formulário inválido: Campo B deve ser maior que Campo A.';
+      mensagem.style.color = 'red';
+    }
+  });
+  
